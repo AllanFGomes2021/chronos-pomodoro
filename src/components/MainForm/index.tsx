47aef315eddba45797c7aacdceb6bceda4a0aff4
AllanFGomes <AllanFGomes2021@gmail.com>
2026-09-23 22:18:56 -0300

    decimo primeiro

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,17 +3,29 @@ import { DefaultInput } from '../DefaultInput';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { useRef } from 'react';
 
 export function MainForm() {
+  //  const [taskName, setTaskName] = useState('');
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('deu');
+  }
+
   return (
     <>
-      <form className='form' action=''>
+      <form onSubmit={handleCreateNewTask} className='form' action=''>
         <div className='formRow'>
           <DefaultInput
             id='meuInput'
             type=''
             labelText='Task'
             placeholder='Digite algo!!!'
+            //            value={taskName}
+            //            onChange={ex => setTaskName(ex.target.value)}
+            ref={taskNameInput}
           />
         </div>
 
